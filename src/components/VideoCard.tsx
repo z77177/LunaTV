c83e175c8942da4da20f686c4b70442a4a0074c4
SzeMeng76 <enquiry@ikmfoundation.org>
2025-09-22 15:47:05 +0800

    Revert "Sync notification border scaling with VideoCard hover animation"
    This reverts commit 92c3b39f40ea05f236106d64cf3546d0559860e6.

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -503,7 +503,7 @@ const VideoCard = forwardRef<VideoCardHandle, VideoCardProps>(function VideoCard
   return (
     <>
       <div
-        className='group relative w-full rounded-lg bg-transparent cursor-pointer transition-transform duration-200 ease-out hover:scale-105 hover:z-[500]'
+        className='group relative w-full rounded-lg bg-transparent cursor-pointer transition-transform duration-200 ease-out group-hover:scale-105 hover:z-[500]'
         onClick={handleClick}
         {...longPressProps}
         style={{
