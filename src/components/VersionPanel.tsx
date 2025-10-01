f86f6be5a6041da4d37ef427b7a87fe347b6c99c
WeiBreeze <xwf8188@gmail.com>
2025-10-01 09:05:59 +0800

    Update VersionPanel.tsx

--- src/components/VersionPanel.tsx
+++ src/components/VersionPanel.tsx
@@ -363,7 +363,7 @@ export const VersionPanel: React.FC<VersionPanelProps> = ({
                     </div>
                   </div>
                   <a
-                    href='https://github.com/SzeMeng76/LunaTV'
+                    href='https://github.com/XWF8188/LunaTV'
                     target='_blank'
                     rel='noopener noreferrer'
                     className='inline-flex items-center justify-center gap-2 px-3 py-2 bg-yellow-600 hover:bg-yellow-700 text-white text-xs sm:text-sm rounded-lg transition-colors shadow-sm w-full'
@@ -393,7 +393,7 @@ export const VersionPanel: React.FC<VersionPanelProps> = ({
                     </div>
                   </div>
                   <a
-                    href='https://github.com/SzeMeng76/LunaTV'
+                    href='https://github.com/XWF8188/LunaTV'
                     target='_blank'
                     rel='noopener noreferrer'
                     className='inline-flex items-center justify-center gap-2 px-3 py-2 bg-green-600 hover:bg-green-700 text-white text-xs sm:text-sm rounded-lg transition-colors shadow-sm w-full'
